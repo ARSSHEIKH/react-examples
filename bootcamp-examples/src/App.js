1555b738e97b45715405f861eebda1bfb095a639
Arsalan <arsheikh665@gmail.com>
2020-09-26 18:32:10 +0500

    added files

---
+++ bootcamp-examples/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+// import Parent from'./parent';
+// import CounterContext from'./CounterContext';
+import ProcessModels from './models/ProcessModels'
+
+function App() {
+  return (
+    // <CounterContext.Provider value="10">
+      // <div>
+      //   <Parent name="Arsalan"/>
+      // </div>
+    // </CounterContext.Provider>
+      <div>
+        <ProcessModels/>
+      </div>
+
+
+  );
+}
+
+export default App;
